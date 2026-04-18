dfb6357b743b11662f8bcf15d0005562a80d14ec
ronaldolimarj77 <roliver.webdeveloper@gmail.com>
2026-04-18 16:32:22 -0300

    Create Service entity class for holistic services

---
+++ src/main/java/com/holistic/entity/Service.java
@@ -0,0 +1,48 @@
+package com.holistic.entity;
+
+public class Service {
+
+    private String name;
+    private String description;
+    private double price;
+    private String duration;
+
+    public Service(String name, String description, double price, String duration) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+}
