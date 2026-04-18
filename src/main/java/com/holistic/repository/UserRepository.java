cfef551b68407706c02dce90f1bdea9a122dee6a
ronaldolimarj77 <roliver.webdeveloper@gmail.com>
2026-04-18 16:34:11 -0300

    Create UserRepository.java

---
+++ src/main/java/com/holistic/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.holistic.repository;
+
+import com.holistic.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
