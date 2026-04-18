9c06b95131c4152d40c9a877235fff381cf18c20
ronaldolimarj77 <roliver.webdeveloper@gmail.com>
2026-04-18 16:32:08 -0300

    Create Review entity class for user ratings and comments

---
+++ src/main/java/com/holistic/entity/Review.java
@@ -0,0 +1,29 @@
+package com.holistic.entity;
+
+public class Review {
+    private int rating;
+    private String comment;
+
+    public Review() {}
+
+    public Review(int rating, String comment) {
+        this.rating = rating;
+        this.comment = comment;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
